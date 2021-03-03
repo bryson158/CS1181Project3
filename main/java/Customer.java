6629fcf4a039d9724f901281b6df2c15b6ecf66d
bryson <bryson158>
2021-03-03 13:19:02 -0500

    Third Commit

--- main/java/Customer.java
+++ main/java/Customer.java
@@ -1,11 +1,34 @@
+import java.text.DecimalFormat;
+
 public class Customer {
     private final double arrival;
     private final int items;
-    private final int timeShopping;
+    private final double timeShopping;
 
-    public Customer(double arrival, int items, int timeShopping) {
+    public Customer(double arrival, int items, double timeShopping) {
         this.arrival = arrival;
         this.items = items;
-        this.timeShopping = timeShopping * items;
+        this.timeShopping = (timeShopping * items);
+    }
+
+    public double getArrival() {
+        return arrival;
+    }
+
+    public int getItems() {
+        return items;
+    }
+
+    public double getTimeShopping() {
+        return timeShopping;
+    }
+
+    @Override
+    public String toString() {
+        return "Customer{" +
+                "arrival=" + arrival +
+                ", items=" + items +
+                ", timeShopping=" + timeShopping +
+                '}';
     }
 }
--- main/java/Simulation.java
+++ main/java/Simulation.java
@@ -1,37 +1,49 @@
-import java.io.File;
-import java.io.FileNotFoundException;
 import java.io.FileReader;
+import java.io.IOException;
 import java.util.ArrayList;
 import java.util.PriorityQueue;
 import java.util.Queue;
 import java.util.Scanner;
 
 public class Simulation {
-    public static void main(String[] args){
-        readInData();
+    public static void main(String[] args) throws IOException {
+        simClock();
     }
 
-    //Reads in the information from the text file
-    public static void readInData(){
-        File textFile = new File("arrival.txt");
+    ArrayList<Customer> allCustomers = new ArrayList<>();
 
-        //Catches the exception for not finding the file
-        try {
-            Scanner input = new Scanner(new FileReader(textFile));
-            while (input.hasNext()){
-                System.out.println(input.next());
-            }
-        } catch (FileNotFoundException e) {
-            e.printStackTrace();
-            System.out.println("The file was not found");
-            System.exit(1);
+    //Reads in the information from the text file
+    public static ArrayList<Customer> readInCustomers() throws IOException {
+        //For some reason this was requiring an absolute path in order to run the program. I'm not entirely sure why
+        //TODO - Remove the absolute path and add a relative path and test it.
+        Scanner input = new Scanner(new FileReader("G:\\CS1181\\Lab\\Projects\\Project 3\\src\\main\\java\\arrival.txt"));
+
+        ArrayList<Customer> customers = new ArrayList<>();
+
+        while (input.hasNext()){
+            double arrivalTime = input.nextDouble();
+            int numItems = input.nextInt();
+            double selectionTime = input.nextDouble();
+
+            customers.add(new Customer(arrivalTime, numItems, selectionTime));
         }
+        return customers;
     }
 
     //Runs the sim
-    public static void SimClock(){
+    public static void simClock() throws IOException {
         double simTime = 0.00;
         Queue events = new PriorityQueue();
+
+        //List for the customers in the store.
         ArrayList<Customer> customersInStore = new ArrayList<>();
+
+        //List of all customers for the day.
+        ArrayList<Customer> allCustomersForDay = new ArrayList<>();
+        allCustomersForDay = readInCustomers();
+
+        for (Customer c: allCustomersForDay) {
+            System.out.println(c.toString());
+        }
     }
 }
